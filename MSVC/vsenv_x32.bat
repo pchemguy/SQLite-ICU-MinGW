a9a86ba7f41579e194f77f9d8cb91c2d23292362
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2021-12-24 13:56:52 +0300

    MS BuildTools scripts

--- MSVC/vsenv_x32.bat
+++ MSVC/vsenv_x32.bat
@@ -1,26 +1,9 @@
 @echo off
 :: ============================================================================
 :: Starts Microsoft Visual C++ Build Tools (MSVC toolset) shell.
-::
-:: Addc ICU4C and TCL to the environment:
-::   Expected TCL bin location: %PREFIX%\TCL\bin
-::   Expected ICU4C-x32 bin location: %PREFIX%\icu4c\bin
-::   Expected ICU4C-x64 bin location: %PREFIX%\icu4c\bin64
-:: PREFIX points to %ProgramFiles% junction having the same name w/o spaces.
 :: ============================================================================
 
 set ARCH=32
-if %ARCH%==32 (
-  set ICUARCH=
-) else (
-  set ICUARCH=64
-)
-
-set PREFIX=%ProgramFiles: =%
-
-set INCLUDE=%PREFIX%\icu4c\include;%INCLUDE%
-set Path=%PREFIX%\icu4c\bin%ICUARCH%;%PREFIX%\TCL\bin;%Path%
-set LIB=%PREFIX%\icu4c\lib%ICUARCH%;%LIB%
 
 call "%~dp0VC\Auxiliary\Build\vcvars%ARCH%.bat"
 
--- MSVC/vsenv_x64.bat
+++ MSVC/vsenv_x64.bat
@@ -1,18 +1,11 @@
 @echo off
 :: ============================================================================
 :: Starts Microsoft Visual C++ Build Tools (MSVC toolset) shell.
-::
-:: Addc ICU4C and TCL to the environment:
-::   Expected TCL bin location: %ProgramFiles%\TCL\bin
-::   Expected ICU4C-x32 bin location: %ProgramFiles%\icu4c\bin
-::   Expected ICU4C-x64 bin location: %ProgramFiles%\icu4c\bin64
 :: ============================================================================
 
-set INCLUDE=%ProgramFiles%\icu4c\include;%INCLUDE%
-set Path=%ProgramFiles%\icu4c\bin64;%ProgramFiles%\TCL\bin;%Path%
-set LIB=%ProgramFiles%\icu4c\lib64;%LIB%
+set ARCH=64
 
-call "%~dp0VC\Auxiliary\Build\vcvars64.bat"
+call "%~dp0VC\Auxiliary\Build\vcvars%ARCH%.bat"
 
 if exist "%~1" (
   cd /d "%~1"
