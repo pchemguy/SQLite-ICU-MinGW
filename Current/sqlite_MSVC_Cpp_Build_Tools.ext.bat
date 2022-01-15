537e127de778e63f61ae7c9e3c7da4386989b65e
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2022-01-15 21:36:13 +0300

    SQLite - MSVC

--- Current/sqlite_MSVC_Cpp_Build_Tools.ext.bat
+++ Current/sqlite_MSVC_Cpp_Build_Tools.ext.bat
@@ -167,13 +167,13 @@ exit /b 0
 if not defined USE_ICU (set USE_ICU=1)
 if not %USE_ICU% EQU 1 (exit /b 0)
 
-if "/%VSCMD_ARG_HOST_ARCH%/"=="/x86/" (set "ARCHX=") else (set ARCHX=64)
+if "/%VSCMD_ARG_TGT_ARCH%/"=="/x86/" (set "ARCH=") else (set ARCH=64)
 for /f "usebackq" %%I in (`where uconv 2^>nul`) do (set UCONV=%%I)
 if not "/%UCONV%/"=="//" (
-  if not defined ICU_HOME (set ICU_HOME=%UCONV:\bin!ARCHX!\uconv.exe=%)
+  if not defined ICU_HOME (set ICU_HOME=%UCONV:\bin!ARCH!\uconv.exe=%)
 )
 if "/%ICU_HOME%/"=="//" (set ICU_HOME=%ProgramFiles%\icu4c)
-if not exist "%ICU_HOME%\bin%ARCHX%\uconv.exe" (
+if not exist "%ICU_HOME%\bin%ARCH%\uconv.exe" (
   echo ICU binaries not found, disabling the ICU extension.
   set USE_ICU=0
   exit /b 0
@@ -182,8 +182,8 @@ if not exist "%ICU_HOME%\bin%ARCHX%\uconv.exe" (
 set ICU_HOME=%ICU_HOME: =%
 set ICUDIR=%ICU_HOME%
 set ICUINCDIR=%ICUDIR%\include
-set ICULIBDIR=%ICUDIR%\lib%ARCHX%
-set ICUBINDIR=%ICUDIR%\bin%ARCHX%
+set ICULIBDIR=%ICUDIR%\lib%ARCH%
+set ICUBINDIR=%ICUDIR%\bin%ARCH%
 set INCLUDE=%ICUINCDIR%;%INCLUDE%
 set Path=%ICUBINDIR%;%Path%
 set LIB=%ICULIBDIR%;%LIB%
