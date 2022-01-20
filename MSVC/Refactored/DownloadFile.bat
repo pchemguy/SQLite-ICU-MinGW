6988b2d437f6263df0bc7b0345188cbf84f168fd
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2022-01-21 02:02:02 +0300

    Extracts filename from URL

--- MSVC/Refactored/DownloadFile.bat
+++ MSVC/Refactored/DownloadFile.bat
@@ -6,13 +6,14 @@
 ::
 :: Arguments:
 ::   %1 - URL
-::   %2 - File name
+::   %2 - File name (optional; if not provided, tries to extract the last part of the URL)
 ::
 :: On failure:
 ::   ResultCode <> 0
 ::
 :: Examples:
 ::   DownloadFile.bat https://github.com/unicode-org/icu/releases/download/release-70-1/icu4c-70_1-Win32-MSVC2019.zip icu4c-70_1-Win32-MSVC2019.zip
+::   DownloadFile.bat https://github.com/unicode-org/icu/releases/download/release-70-1/icu4c-70_1-Win32-MSVC2019.zip
 ::
 echo.
 echo ==================== Downloading file ====================
@@ -22,17 +23,14 @@ if "/%FileURL%/"=="//" (
   echo File URL is not supplied.
   set ResultCode=1
 )
-set FileName=%~2
-if "/%FileName%/"=="//" (
-  echo File name is not supplied.
-  set ResultCode=1
-)
 if not %ResultCode% EQU 0 (
   echo Correct arguments have not been provided to download file.
   echo ----------------------------------------------------------
   echo.
   exit /b %ResultCode%
 )
+set FileName=%~2
+if "/%FileName%/"=="//" (call :EXTRACT_FILENAME %FileURL%)
 
 if not exist "%FileName%" (
   echo ===== Downloading %FileName% =====
@@ -50,3 +48,17 @@ echo ----------------------------------------------------------
 echo.
 
 exit /b %ResultCode%
+
+
+:: ============================================================================
+:EXTRACT_FILENAME
+set URL=%~1
+set URL=%URL:http://=%
+set URL=%URL:https://=%
+set URL=%URL:?= %
+call :SET_FILENAME %URL%
+exit /b 0
+
+:SET_FILENAME
+set FileName=%~nx1
+exit /b 0
