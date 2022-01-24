18b1063dd700d6173d216a3365e6b68e13174e0e
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2022-01-24 17:22:24 +0300

    Cleanup

--- MSVC/Refactored/OpenSSLGet.bat
+++ MSVC/Refactored/OpenSSLGet.bat
@@ -2,25 +2,30 @@
 ::
 :: Prepares the OpenSSL library and sets build flags.
 ::
+if "/%VSCMD_ARG_TGT_ARCH%/"=="/x64/" (set "ARCH=x64" & set "SYSCONF=VC-WIN64A")
+if "/%VSCMD_ARG_TGT_ARCH%/"=="/x86/" (set "ARCH=x32" & set "SYSCONF=VC-WIN32")
+
 set BASEDIR=%~dp0
 set BASEDIR=%BASEDIR:~0,-1%
 set PKGDIR=%BASEDIR%\pkg
 set BLDDIR=%BASEDIR%\bld
 set DEVDIR=%BASEDIR%\dev
-set HOMSSL=%BASEDIR%\dev\openssl
-set BLDSSL=%BASEDIR%\bld\openssl
-set OUTSSL="%BASEDIR%\opensslout.log"
-set ERRSSL="%BASEDIR%\opensslerr.log"
+set HOMSSL=%BASEDIR%\dev\openssl\%ARCH%
+set SRCSSL=%BASEDIR%\bld\openssl\src
+set BLDSSL=%BASEDIR%\bld\openssl\%ARCH%
+set OUTSSL="%BLDSSL%\opensslout.log"
+set ERRSSL="%BLDSSL%\opensslerr.log"
 del %OUTSSL% 2>nul
 del %ERRSSL% 2>nul
 set ResultCode=0
 
+
 call "%~dp0GNUGet.bat"
-if not "/%ErrorLevel%/"=="/0/" (set ResultCode=%ErrorLevel%)
+if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
 call "%~dp0PerlGet.bat"
-if not "/%ErrorLevel%/"=="/0/" (set ResultCode=%ErrorLevel%)
+if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
 call "%~dp0NASMGet.bat"
-if not "/%ErrorLevel%/"=="/0/" (set ResultCode=%ErrorLevel%)
+if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
 
 if not exist "%PKGDIR%" mkdir "%PKGDIR%"
 pushd "%PKGDIR%"
@@ -44,73 +49,63 @@ call "%~dp0DownloadFile.bat" %URLSSL%
 if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
 
 :: Extract
-if not exist "%BLDSSL%-src\Configure" (
-  if exist "%BLDSSL%-src" rmdir /S /Q "%BLDSSL%-src" 2>nul
+if not exist "%SRCSSL%\Configure" (
+  if exist "%SRCSSL%" rmdir /S /Q "%SRCSSL%" 1>nul
+  mkdir "%SRCSSL:~0,-4%" 1>nul
   set TARPATTERN=openssl*
-  call "%~dp0ExtractArchive.bat" %PKGNAM% "%BLDDIR%"
-  if not "/%ErrorLevel%/"=="/0/" (set ResultCode=%ErrorLevel%)
-  cd /d "%BLDDIR%"
-  move "%PKGNAM:.tar.gz=%" "openssl-src"
+  call "%~dp0ExtractArchive.bat" %PKGNAM% "%SRCSSL%\.."
+  if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
+  cd /d "%SRCSSL%\.."
+  move "%PKGNAM:.tar.gz=%" "src" 1>nul
 )
 
-if "/%VSCMD_ARG_TGT_ARCH%/"=="/x64/" (set "ARCH=x64" & set "SYSCONF=VC-WIN64A")
-if "/%VSCMD_ARG_TGT_ARCH%/"=="/x86/" (set "ARCH=x32" & set "SYSCONF=VC-WIN32")
-
 :: Configure
-set BLDBLD=%BLDSSL%-bld\%ARCH%
-set HOMHOM=%HOMSSL%\%ARCH%
-(
-  if not exist "%BLDBLD%\makefile" (
-    if exist "%BLDBLD%" rmdir /S /Q "%BLDBLD%" 2>nul
-    mkdir "%BLDBLD%" 2>nul
-    cd /d "%BLDBLD%"
-    echo ===== Configuring OpenSSL =====
-    perl "%BLDSSL%-src\Configure" %SYSCONF% no-tests ^
-      --prefix="%HOMHOM%\core" --openssldir="%HOMHOM%\SSL" 
-    set ResultCode=!ErrorLevel!
-    if !ResultCode! EQU 0 (
-      echo ----- Configured OpenSSL -----
-    ) else (
-      echo Error configuring OpenSSL
-      echo Errod code: !ResultCode!
-    )
+if not exist "%BLDSSL%\makefile" (
+  if exist "%BLDSSL%" rmdir /S /Q "%BLDSSL%" 1>nul
+  mkdir "%BLDSSL%" 1>nul
+  cd /d "%BLDSSL%"
+  echo ===== Configuring OpenSSL =====
+  perl "%SRCSSL%\Configure" %SYSCONF% no-tests --prefix="%HOMSSL%\core" ^
+    --openssldir="%HOMSSL%\SSL" 1>>%OUTSSL% 2>>%ERRSSL%
+  set ResultCode=!ErrorLevel!
+  if !ResultCode! EQU 0 (
+    echo ----- Configured OpenSSL -----
   ) else (
-    echo ===== Using previously configured OpenSSL setup =====
+    echo Error configuring OpenSSL
+    echo Errod code: !ResultCode!
   )
-) 1>>%OUTSSL% 2>>%ERRSSL%
+) else (
+  echo ===== Using previously configured OpenSSL setup =====
+)
 
 :: Make
-( 
-  if not exist "%BLDBLD%\libcrypto-3.dll" (
-    cd /d "%BLDBLD%"
-    echo ===== Making OpenSSL =====
-    nmake
-    if %ErrorLevel% EQU 0 (
-      echo ----- Made OpenSSL -----
-    ) else (
-      set ResultCode=%ErrorLevel%
-      echo Error making OpenSSL
-      echo Errod code: !ResultCode!
-    )
+if not exist "%BLDSSL%\libcrypto-3.dll" (
+  cd /d "%BLDSSL%"
+  echo ===== Making OpenSSL =====
+  nmake 1>>%OUTSSL% 2>>%ERRSSL%
+  if %ErrorLevel% EQU 0 (
+    echo ----- Made OpenSSL -----
   ) else (
-    echo ===== Using previously made OpenSSL =====
+    set ResultCode=%ErrorLevel%
+    echo Error making OpenSSL
+    echo Errod code: !ResultCode!
   )
-) 1>>%OUTSSL% 2>>%ERRSSL%
+) else (
+  echo ===== Using previously made OpenSSL =====
+)
 
 :: Install
-( 
-  if not exist "%HOMHOM%\core\lib\libcrypto.lib" (
-    echo ===== Installing OpenSSL =====
-    cd /d "%BLDBLD%"
-    if exist "%HOMHOM%" rmdir /S /Q "%HOMHOM%" 2>nul
-    mkdir "%HOMHOM%" 2>nul
-    nmake install
-    copy /Y "%BLDBLD%\lib*_static.lib" "%HOMHOM%\core\lib"
-    echo ----- Installed OpenSSL -----
-  ) else (
-    echo ===== Using previously installed OpenSSL =====
-  )
-) 1>>%OUTSSL% 2>>%ERRSSL%
+if not exist "%HOMSSL%\core\lib\libcrypto.lib" (
+  echo ===== Installing OpenSSL =====
+  cd /d "%BLDSSL%"
+  if exist "%HOMSSL%" rmdir /S /Q "%HOMSSL%" 1>nul
+  mkdir "%HOMSSL%" 1>nul
+  nmake install 1>>%OUTSSL% 2>>%ERRSSL%
+  copy /Y "%BLDSSL%\lib*_static.lib" "%HOMSSL%\core\lib" 1>nul
+  echo ----- Installed OpenSSL -----
+) else (
+  echo ===== Using previously installed OpenSSL =====
+)
 
 
 echo.
@@ -120,11 +115,11 @@ echo.
 
 
 :: Set building flags
-:: /LIBPATH:"%HOMHOM%\core\lib"
-set OpenSSL_LIBPATH=%HOMHOM%\core\lib
+:: /LIBPATH:"%HOMSSL%\core\lib"
+set OpenSSL_LIBPATH=%HOMSSL%\core\lib
 if "/!LIBPATH!/"=="/!LIBPATH:%OpenSSL_LIBPATH%=!/" set LIBPATH=%OpenSSL_LIBPATH%;%LIBPATH%
-:: -I"%HOMHOM%\core\include"
-set OpenSSL_INCLUDE=%HOMHOM%\core\include
+:: -I"%HOMSSL%\core\include"
+set OpenSSL_INCLUDE=%HOMSSL%\core\include
 if "/!INCLUDE!/"=="/!INCLUDE:%OpenSSL_INCLUDE%=!/" set INCLUDE=%OpenSSL_INCLUDE%;%INCLUDE%
 set OpenSSL_LIB_IMPORT=libcrypto.lib libssl.lib
 set OpenSSL_LIB_STATIC=libcrypto_static.lib libssl_static.lib
@@ -139,12 +134,11 @@ echo ----------------------------------------
 
 :: Cleanup
 set HOMSSL=
+set SRCSSL=
 set BLDSSL=
 set OUTSSL=
 set ERRSSL=
 set InfoURL=
-set BLDBLD=
-set HOMHOM=
 
 popd
 
