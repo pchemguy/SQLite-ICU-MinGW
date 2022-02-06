d049dd45b9c83dbda7a5a678723d71b794e3b7c7
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2022-02-06 22:29:08 +0300

    SQLite-SQLCipher - MSVC

---
+++ DevEnvScripts/sql.bat
@@ -0,0 +1,2 @@
+::                 0/1                0/1                    0/1               0/1/"#N/D"        sqlite/sqlcipher            dll/shelldll
+set "WITH_EXTRA_EXT=1" & set "USE_ZLIB=1" & set "USE_LIBSHELL=0" & set "USE_STDCALL=" & set "DBENG=sqlite" & sqlcipher_MSVC.bat dll
--- DevEnvScripts/sqlcipher_MSVC.bat
+++ DevEnvScripts/sqlcipher_MSVC.bat
@@ -23,6 +23,13 @@ exit /b 0
 :MAIN
 SetLocal EnableExtensions EnableDelayedExpansion
 
+call "%~dp0MSYS2Get.bat" %DBENG%
+set ResultCode=!ErrorLevel!
+if not "/!ResultCode!/"=="/0/" (
+  echo MSYS2Get.bat error.
+  exit /b !ResultCode!
+)
+
 if "/%VSCMD_ARG_TGT_ARCH%/"=="/x64/" (set ARCH=x64)
 if "/%VSCMD_ARG_TGT_ARCH%/"=="/x86/" (set ARCH=x32)
 if /I not "/%DBENG:~0,3%/"=="/sql/" set DBENG=sqlite
@@ -41,7 +48,6 @@ set ERRSQL="%BLDSQL%\sqlerr.log"
 del %OUTSQL% 2>nul
 del %ERRSQL% 2>nul
 set ResultCode=0
-set ERROR_STATUS=0
 
 if not defined USE_STDCALL (
   if "/%ARCH%/"=="/x32/" (set USE_STDCALL=1) else (set USE_STDCALL=0)
@@ -58,7 +64,11 @@ echo USE_SQLAR=%USE_SQLAR%
 echo USE_LIBSHELL=%USE_LIBSHELL%
 
 call "%~dp0SQLiteCipherSourceGet.bat" %DBENG%
-if %ErrorLevel% NEQ 0 exit /b 1
+set ResultCode=!ErrorLevel!
+if not "/!ResultCode!/"=="/0/" (
+  echo SQLiteCipherSourceGet.bat error.
+  exit /b !ResultCode!
+)
 if not exist "%BLDSQL%" (
   echo Distro directory does not exists. Exiting
   exit /b 1
@@ -97,8 +107,8 @@ if %WITH_EXTRA_EXT% EQU 1 (
   call :TEST_MAIN_C_SQLITE3_H
   call :EXT_MAIN
   call :EXT_NORMALIZE
-  call :EXT_REGEXP
   call :EXT_SHA1
+  call :EXT_BASE_PATCH REGEXP
   call :EXT_BASE_PATCH CSV
   call :EXT_BASE_PATCH SERIES
   call :EXT_BASE_PATCH FILEIO
@@ -171,11 +181,25 @@ exit /b 0
 echo ===== Setting options =====
 :: TCL/Tk
 call "%~dp0TCLTkGet.bat"
-if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
+set ResultCode=!ErrorLevel!
+if not "/!ResultCode!/"=="/0/" (
+  echo TCLTkGet.bat error.
+  exit /b !ResultCode!
+)
 :: ICU
+:: Requires icudtXX.dll icuucXX.dll icuinXX.dll
 if not defined USE_ICU (set USE_ICU=1)
-call "%~dp0ICUGet.bat"
-if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
+if %USE_ICU% EQU 1 (
+  call "%~dp0ICUGet.bat"
+  set ResultCode=!ErrorLevel!
+  if not "/!ResultCode!/"=="/0/" (
+    echo ICUGet.bat error.
+    exit /b !ResultCode!
+  )
+  set ICUDIR=!ICU_ROOT!
+  set ICUINCDIR=!ICU_INCLUDE!
+  set ICULIBDIR=!ICU_LIBPATH!
+)
 
 if not defined WITH_EXTRA_EXT set WITH_EXTRA_EXT=1
 if %WITH_EXTRA_EXT% EQU 1 (
@@ -192,8 +216,16 @@ echo ===== Setting ZLIB options =====
 if %USE_ZLIB% EQU 1 (
   set BUILD_ZLIB=0
   call "%~dp0ZLibGet.bat"
-  if not "/!ErrorLevel!/"=="/0/" exit /b !ErrorLevel!
+  set ResultCode=!ErrorLevel!
+  if not "/!ResultCode!/"=="/0/" (
+    echo ZLibGet.bat error.
+    exit /b !ResultCode!
+  )
   set ZLIBLIB=!ZLIB_LIBIMPORT!
+  set ZLIBDIR=!ZLIB_ROOT!
+  set ZLIBINCDIR=!ZLIB_INCLUDE!
+  set ZLIBLIBDIR=!ZLIB_LIBPATH!
+  set CCOPTS=%CCOPTS% !ZLIB_CFLAGS!
 )
 
 call :BUILD_OPTIONS
@@ -205,17 +237,14 @@ exit /b 0
 
 :: ============================================================================
 :SQLCIPHER_OPTIONS
-:: NASM
-call "%~dp0NASMGet.bat"
-if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
-:: PERL
-call "%~dp0OpenSSLGet.bat"
-if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
-
 ::OPENSSL_OPTIONS
 echo ===== Setting OpenSSL options =====
 call "%~dp0OpenSSLGet.bat"
-if not "/%ErrorLevel%/"=="/0/" exit /b %ErrorLevel%
+set ResultCode=!ErrorLevel!
+if not "/!ResultCode!/"=="/0/" (
+  echo OpenSSLGet.bat error.
+  exit /b !ResultCode!
+)
 set CCOPTS=%CCOPTS% -I"%OSSL_INCLUDE%"
 set LTLIBPATHS=%LTLIBPATHS% /LIBPATH:"%OSSL_LIBPATH%"
 set LTLIBS=%LTLIBS% %OSSL_LIBIMPORT%
@@ -311,7 +340,7 @@ set FILENAME=%BLDSQL%\Makefile.msc
 if %USE_LIBSHELL% EQU 1 (
   set LIBSHELLEXT="/shell:\s*\$.SQLITE3EXE.$/r %FILENAME%.libshell"
 ) else (set LIBSHELLEXT=";")
-sed -e "s/^TOP = .$/TOP = %SRCSQL:\=\\%/" ^
+sed -e "s/^TOP = .$/TOP = %SRCSQL:\=\\\\%/" ^
     -e "s/\(DSQLITE_TEMP_STORE=\)1/\1%DSQLITE_TEMP_STORE%/g" ^
     -e "s/\(win32\\Makefile.msc\) clean/\1 LOC=$(ZLIBLOC) clean/" ^
     -e %LIBSHELLEXT% ^
@@ -400,7 +429,9 @@ exit /b 0
 :EXT_ADD_SOURCES_TO_MAKEFILE_MSC
 set FILENAME=%BLDSQL%\Makefile.msc
 echo ========== Patching "%FILENAME%" ===========
-sed -i "%FILENAME%" -e "/^  \$(TOP)\\ext\\rtree\\rtree.c \\$/r %FILENAME%.ext"
+set MATCH=  $(TOP)\ext\rtree\rtree.c \
+set MATCH=%MATCH:\=\\\\%
+sed -i "%FILENAME%" -e "/^%MATCH%$/r %FILENAME%.ext"
 
 exit /b 0
 
@@ -421,7 +452,7 @@ exit /b 0
 :EXT_MAIN
 set FILENAME=%BLDSQL%\tsrc\main.c
 echo ========== Patching "%FILENAME%" ===========
-sed -i "%FILENAME%" -e "/^int sqlite3Json1Init/r %FILENAME%.1.ext"
+sed -i "%FILENAME%" -e "/^int sqlite3Fts5Init/r %FILENAME%.1.ext"
 sed -i "%FILENAME%" -e "/^  sqlite3RtreeInit,$/r %FILENAME%.2.ext"
 
 exit /b 0
@@ -446,25 +477,9 @@ exit /b 0
 :EXT_SHA1
 cd /d "%BLDSQL%\tsrc"
 set FILENAME=sha1.c
-set OLDTEXT=hash_step_vformat
-set NEWTEXT=hash_step_vformat_sha1
-tclsh "%BASEDIR%\scripts\replace.tcl" "%OLDTEXT%" "%NEWTEXT%" "%FILENAME%"
+sed -i "%FILENAME%" -e "s/\(hash_step_vformat\)/\1_sha1/"
+
 call :EXT_BASE_PATCH SHA sha1.c
-
-exit /b 0
-
-
-:: ============================================================================
-:EXT_REGEXP
-cd /d "%BLDSQL%\tsrc"
-set FILENAME=regexp.c
-set OLDTEXT=#include ^<string.h^>
-set NEWTEXT=#include ^<sqlite3ext.h^>
-tclsh "%BASEDIR%\scripts\replace.tcl" "%OLDTEXT%" "%NEWTEXT%" "%FILENAME%"
-set OLDTEXT=#include \"sqlite3ext.h\"
-set NEWTEXT=#include ^<string.h^>
-tclsh "%BASEDIR%\scripts\replace.tcl" "%OLDTEXT%" "%NEWTEXT%" "%FILENAME%"
-call :EXT_BASE_PATCH REGEXP
 
 exit /b 0
 
@@ -473,18 +488,15 @@ exit /b 0
 :EXT_WINDIRENT
 cd /d "%BLDSQL%"
 if %USE_LIBSHELL% EQU 0 (
-  copy tsrc\test_windirent.c .
-  copy tsrc\test_windirent.h .
+  copy tsrc\test_windirent.* .
   copy tsrc\fileio.c .
   set FILENAME=test_windirent.c
   echo ========== Patching "!FILENAME!" ===========
-  tclsh "%BASEDIR%\scripts\expandinclude.tcl" "!FILENAME!" "test_windirent.h" .
-  (
-    echo #include "test_windirent.c"
-    echo #include "fileio.c"
-  ) >>sqlite3.c
-  tclsh "%BASEDIR%\scripts\expandinclude.tcl" "sqlite3.c" "test_windirent.c" .
-  tclsh "%BASEDIR%\scripts\expandinclude.tcl" "sqlite3.c" "fileio.c" .
+  call :EXPAND_INCLUDE "!FILENAME!" "test_windirent.h"
+  (echo #include "test_windirent.c"
+   echo #include "fileio.c" ) >>sqlite3.c
+  call :EXPAND_INCLUDE "sqlite3.c" "test_windirent.c"
+  call :EXPAND_INCLUDE "sqlite3.c" "fileio.c"
 )
 
 exit /b 0
@@ -492,18 +504,15 @@ exit /b 0
 
 :: ============================================================================
 :EXT_ZIPFILE
-cd /d "%BLDSQL%\tsrc"
 set FLAG=SQLITE_ENABLE_ZIPFILE
-set FILENAME=zipfile.c
+set FILENAME=%BLDSQL%\tsrc\zipfile.c
 echo ========== Patching "%FILENAME%" ===========
-set OLDTEXT=static int zipfileRegister
-set NEWTEXT=int zipfileRegister
-tclsh "%BASEDIR%\scripts\replace.tcl" "%OLDTEXT%" "%NEWTEXT%" "%FILENAME%"
-set OLDTEXT=#include \"sqlite3ext.h\"
-set NEWTEXT=#if defined(%FLAG%)\n\n#include \"sqlite3ext.h\"
-tclsh "%BASEDIR%\scripts\replace.tcl" "%OLDTEXT%" "%NEWTEXT%" "%FILENAME%"
+sed -e "s/^static \(int zipfileRegister\)/\1/" ^
+    -e "/^#include .sqlite3ext.h./i #ifdef %FLAG%\n" ^
+    -i "%FILENAME%"
+
 echo. >>"%FILENAME%"
-echo #endif /* defined^(%FLAG%^) */ >>"%FILENAME%"
+echo #endif /* defined(%FLAG%) */ >>"%FILENAME%"
 
 exit /b 0
 
@@ -514,19 +523,17 @@ exit /b 0
 ::   - %1 - FLAG suffix
 ::   - %2 - FILENAME (if omitted, use %1.c)
 set FLAG=SQLITE_ENABLE_%~1
-if "/%~2/"=="//" (set FILENAME=%~1.c) else (set FILENAME=%~2)
+if "/%~2/"=="//" (
+  set CommandText=echo %~1.c^| tr '[:upper:]' '[:lower:]'
+  for /f "Usebackq delims=" %%G in (`!CommandText!`) do (set FILENAME=%%~G)
+) else (set FILENAME=%~2)
+
 echo ========== Patching "%FILENAME%" ===========
-if exist "%FILENAME%.ext" (
-  tclsh "%BASEDIR%\scripts\addlines.tcl" "%FILENAME%" "%FILENAME%.ext" .
-)
-set OLDTEXT=#include ^<sqlite3ext.h^>
-set NEWTEXT=#include \"sqlite3ext.h\"
-tclsh "%BASEDIR%\scripts\replace.tcl" "%OLDTEXT%" "%NEWTEXT%" "%FILENAME%"
-set OLDTEXT=#include \"sqlite3ext.h\"
-set NEWTEXT=#if defined(%FLAG%)\n\n#include \"sqlite3ext.h\"
-tclsh "%BASEDIR%\scripts\replace.tcl" "%OLDTEXT%" "%NEWTEXT%" "%FILENAME%"
-echo. >>"%FILENAME%"
-echo #endif /* defined^(%FLAG%^) */ >>"%FILENAME%"
+( echo #if defined^(%FLAG%^) & echo. & type "%FILENAME%"
+  if exist "%FILENAME%.ext" type "%FILENAME%.ext"
+  echo. & echo #endif /* defined^(%FLAG%^) */
+) >>"%FILENAME%.tmp"
+move "%FILENAME%.tmp" "%FILENAME%"
 
 exit /b 0
 
@@ -544,18 +551,15 @@ echo #if ^^!defined^^(LIBSHELL_C^^) ^&^& defined^^(%FLAG%^^) >%FILENAME%
 echo #define LIBSHELL_C >>%FILENAME%
 type shell.c >>%FILENAME%
 
-set MAPLIST=^
-`int SQLITE_CDECL main` `int SQLITE_CDECL libshell_main` ^
-`appendText` `shAppendText`
+sed -e "s/^\(int SQLITE_CDECL\) main/\1 libshell_main/" ^
+    -e "s/appendText/shAppendText/g" ^
+    -i "%FILENAME%"
 
-set MAPLIST=%MAPLIST:`="%
-tclsh "%BASEDIR%\scripts\replace_multi.tcl" "%FILENAME%" %MAPLIST%
-
-echo. >>%FILENAME%
-echo. >>%FILENAME%
-type libshell.c.ext >>%FILENAME%
-echo. >>%FILENAME%
-echo. >>%FILENAME%
+(echo.
+ echo.
+ type libshell.c.ext
+ echo.
+ echo.) >>%FILENAME%
 echo #endif /* ^^!defined^^(LIBSHELL_C^^) ^&^& defined^^(%FLAG%^^) */ >>%FILENAME%
 
 if /I "/%TARGETS%/"=="/dll/" (set TARGETS=shelldll) else (
@@ -575,9 +579,38 @@ cd /d "%BINSQL%"
 if exist "%BLDSQL%\sqlite3.dll" move "%BLDSQL%\sqlite3.dll" .
 if exist "%BLDSQL%\sqlite3.exe" move "%BLDSQL%\sqlite3.exe" .
 if exist "%BLDSQL%\sqlite3.def" move "%BLDSQL%\sqlite3.def" .
-if %USE_ICU%  EQU 1 copy /Y "%ICUBIN%\icu*.dll" .
-if %USE_ZLIB% EQU 1 copy /Y "%ZLIBDIR%\zlib1.dll" .
-copy /Y "%OPENSSL_PREFIX%\bin\libcrypto*.dll" .
+if %USE_ICU%  EQU 1 copy /Y "%ICU_BINPATH%\icu*.dll" .
+if %USE_ZLIB% EQU 1 copy /Y "%ZLIB_BINPATH%\zlib1.dll" .
+if /I "/%DBENG%/"=="/sqlcipher/" copy /Y "%OSSL_BINPATH%\lib*.dll" .
 echo ---------- Copied  binaries -----------
+
+exit /b 0
+
+
+:: ============================================================================
+:EXPAND_INCLUDE
+:: Call this sub with two arguments:
+::   - %1 - Source file (may include path)
+::   - %2 - Header file name (file name only)
+::
+setlocal
+set "SOURCE=%~nx1"
+set "HEADER=%~2"
+set "FOLDER=%~dp1"
+if "/%FOLDER%/"=="//" set "FOLDER=."
+pushd "%FOLDER%"
+
+(
+  echo /********************************* BEGIN %HEADER% *********************************/
+  type %HEADER%
+  echo /********************************* END %HEADER% ***********************************/
+) >"%HEADER%.tmp"
+set MATCH=^#\s*include\s*.%HEADER%.\s*$
+
+sed -e "/%MATCH%/r %HEADER%.tmp" -e "/%MATCH%/d" -i "%SOURCE%"
+del /Q "%HEADER%.tmp"
+
+popd
+endlocal
 
 exit /b 0
